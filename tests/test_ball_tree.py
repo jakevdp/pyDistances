fec9636e44294ab9abf2af1942f618d176ca1f7e
Jake Vanderplas <jakevdp@yahoo.com>
2012-07-19 22:10:23 -0700

    TST: refactor ball tree tests

--- tests/test_ball_tree.py
+++ tests/test_ball_tree.py
@@ -9,190 +9,194 @@ import itertools
 
 import cPickle
 
-# dimension for the tests
-DTEST = 10
-
-# create some inverse covariance matrices for mahalanobis
-VI = np.random.random((DTEST, DTEST))
-VI1 = np.dot(VI, VI.T)
-VI2 = np.dot(VI.T, VI)
-
-# For each distance metric, specify a dictionary of ancillary parameters
-#  and a sequence of values to test.
-#
-# note that wminkowski and canberra fail in scipy.spacial.cdist/pdist
-#  in scipy <= 0.8
-
 # create a user-defined metric
 def user_metric(x, y):
     return abs(x - y).sum()
 
-METRIC_DICT = {'euclidean': {},
-               'cityblock': {},
-               'minkowski': dict(p=(1, 1.5, 2.0, 3.0)),
-               'wminkowski': dict(p=(1, 1.5, 2.0),
-                                  w=(np.random.random(DTEST),)),
-               'mahalanobis': dict(VI = (None, VI1, VI2)),
-               'seuclidean': dict(V = (None, np.random.random(DTEST),)),
-               'cosine': {},
-               #'correlation': {},
-               #'hamming': {},
-               'chebyshev': {},
-               'canberra': {},
-               'braycurtis': {},
-               user_metric: {}}
+class TestBallTree:
+    def __init__(self, n_samples=100, n_features=5, rseed=0):
+        np.random.seed(rseed)
+        self.X = np.random.random((n_samples, n_features))
+        self.Xbool = (np.random.random(size=(10, 10)) >= 0.5).astype(float)
+        self.Ybool = (np.random.random(size=(10, 10)) >= 0.5).astype(float)
+        
+        VI = np.random.random((n_features, n_features))
+        VI1 = np.dot(VI, VI.T)
+        VI2 = np.dot(VI.T, VI)
+        w = np.random.rand(n_features)
 
-BOOL_METRIC_DICT = {'yule' : {},
-                    'matching' : {},
-                    'hamming' : {},
-                    'jaccard' : {},
-                    'dice': {},
-                    'kulsinski': {},
-                    'rogerstanimoto': {},
-                    'russellrao': {},
-                    'sokalmichener': {},
-                    'sokalsneath': {}}
+        # For each distance metric, specify a dictionary of ancillary parameters
+        #  and a sequence of values to test.
+        #
+        # note that wminkowski and canberra fail in scipy.spacial.cdist/pdist
+        #  in scipy <= 0.8
+        self.metrics = {'euclidean': {},
+                        'cityblock': {},
+                        'minkowski': dict(p=(1, 1.5, 2.0, 3.0)),
+                        'wminkowski': dict(p=(1, 1.5, 2.0),
+                                           w=(np.random.rand(n_features),)),
+                        'mahalanobis': dict(VI = (None, VI1, VI2)),
+                        'seuclidean': dict(V = (None, w,)),
+                        #'cosine': {},
+                        #'correlation': {},
+                        #'hamming': {},
+                        'chebyshev': {},
+                        'canberra': {},
+                        'braycurtis': {},
+                        user_metric: {}}
 
+        self.bool_metrics = {'yule' : {},
+                             'matching' : {},
+                             'hamming' : {},
+                             'jaccard' : {},
+                             'dice': {},
+                             'kulsinski': {},
+                             'rogerstanimoto': {},
+                             'russellrao': {},
+                             'sokalmichener': {},
+                             'sokalsneath': {}
+                             }
 
-def test_ball_tree_query():
-    X = np.random.random(size=(100, 5))
+    def _check_query_knn(self, bt, kdt, k):
+        dist_bt, ind_bt = bt.query(self.X, k=k)
+        dist_kd, ind_kd = kdt.query(self.X, k=k)
 
-    for k in (2, 4, 6):
+        assert_array_almost_equal(dist_bt, dist_kd)
+        assert_array_almost_equal(ind_bt, ind_kd)
+
+    def test_query_knn(self):
+        bt = BallTree(self.X)
+        kdt = cKDTree(self.X)
+        for k in (2, 4, 6):
+            yield (self._check_query_knn, bt, kdt, k)
+
+    def _check_metrics_float(self, k, metric, kwargs):
+        bt = BallTree(self.X, metric=metric, **kwargs)
+        dist_bt, ind_bt = bt.query(self.X, k=k)
+
+        dm = DistanceMetric(metric=metric, **kwargs)
+        D = dm.pdist(self.X, squareform=True)
+
+        ind_dm = np.argsort(D, 1)[:, :k]
+        dist_dm = D[np.arange(self.X.shape[0])[:, None], ind_dm]
+
+        # we don't check the indices here because if there is a tie for
+        # nearest neighbor, then the test may fail.  Distances will be
+        # in either case.
+        #assert_array_almost_equal(ind_bt, ind_dm)
+
+        assert_array_almost_equal(dist_bt, dist_dm)
+    
+    def test_metrics_float(self, k=5):    
+        for (metric, argdict) in self.metrics.iteritems():
+            keys = argdict.keys()
+            for vals in itertools.product(*argdict.values()):
+                kwargs = dict(zip(keys, vals))
+                yield (self._check_metrics_float, k, metric, kwargs)
+
+    def _check_metrics_bool(self, k, metric, kwargs):
+        bt = BallTree(self.Xbool, metric=metric, **kwargs)
+        dist_bt, ind_bt = bt.query(self.Ybool, k=k)
+
+        dm = DistanceMetric(metric=metric, **kwargs)
+        D = dm.cdist(self.Ybool, self.Xbool)
+
+        ind_dm = np.argsort(D, 1)[:, :k]
+        dist_dm = D[np.arange(self.Ybool.shape[0])[:, None], ind_dm]
+        
+        # we don't check the indices here because there are very often
+        # ties for nearest neighbors, which cause the test to fail.
+        # Distances will be correct in either case.
+        #assert_array_almost_equal(ind_bt, ind_dm)
+
+        assert_array_almost_equal(dist_bt, dist_dm)
+
+    def test_ball_tree_metrics_bool(self, k=3):    
+        for (metric, argdict) in self.bool_metrics.iteritems():
+            keys = argdict.keys()
+            for vals in itertools.product(*argdict.values()):
+                kwargs = dict(zip(keys, vals))
+                yield (self._check_metrics_bool, k, metric, kwargs)
+
+    def _check_p_distance_vs_KDT(self, p):
+        bt = BallTree(self.X, leaf_size=10, metric='minkowski', p=p)
+        kdt = cKDTree(self.X, leafsize=10)
+
+        dist_bt, ind_bt = bt.query(self.X, k=5)
+        dist_kd, ind_kd = kdt.query(self.X, k=5, p=p)
+
+        assert_array_almost_equal(dist_bt, dist_kd)
+
+    def test_p_distance_vs_KDT(self):
+        for p in (1, 2, 3, 4, np.inf):
+            yield (self._check_p_distance_vs_KDT, p)
+
+    def test_query_radius_count(self, n_samples=100, n_features=10):
+        X = 2 * np.random.random(size=(n_samples, n_features)) - 1
+
+        dm = DistanceMetric()
+        D = dm.pdist(X, squareform=True)
+
+        r = np.mean(D)
+
         bt = BallTree(X)
-        kdt = cKDTree(X)
+        count1 = bt.query_radius(X, r, count_only=True)
 
-        dist_bt, ind_bt = bt.query(X, k=k)
-        dist_kd, ind_kd = kdt.query(X, k=k)
+        count2 = (D <= r).sum(1)
 
-        assert_array_almost_equal(dist_bt, dist_kd)
+        assert_array_almost_equal(count1, count2)
 
-def test_ball_tree_metrics_float():
-    X = np.random.random(size=(100, DTEST))
-    k = 5
-    
-    for (metric, argdict) in METRIC_DICT.iteritems():
-        keys = argdict.keys()
-        for vals in itertools.product(*argdict.values()):
-            kwargs = dict(zip(keys, vals))
-            bt = BallTree(X, metric=metric, **kwargs)
-            dist_bt, ind_bt = bt.query(X, k=k)
+    def test_query_radius_indices(self, n_samples=100, n_features=10):
+        X = 2 * np.random.random(size=(n_samples, n_features)) - 1
 
-            dm = DistanceMetric(metric=metric, **kwargs)
-            D = dm.pdist(X, squareform=True)
-            ind_dm = np.argsort(D, 1)[:, :k]
-            dist_dm = D[np.arange(X.shape[0])[:, None], ind_dm]
+        dm = DistanceMetric()
+        D = dm.cdist(X[:10], X)
 
-            try:
-                assert_array_almost_equal(dist_bt, dist_dm)
-            except:
-                print metric, kwargs
-                assert_array_almost_equal(dist_bt, dist_dm)
+        r = np.mean(D)
 
-def test_ball_tree_metrics_bool():
-    X = (np.random.random(size=(10, 10)) >= 0.5).astype(float)
-    Y = (np.random.random(size=(10, 10)) >= 0.5).astype(float)
-    k = 1
-    
-    for (metric, argdict) in BOOL_METRIC_DICT.iteritems():
-        keys = argdict.keys()
-        for vals in itertools.product(*argdict.values()):
-            kwargs = dict(zip(keys, vals))
-            bt = BallTree(X, metric=metric, **kwargs)
-            dist_bt, ind_bt = bt.query(Y, k=k)
+        bt = BallTree(X)
+        ind = bt.query_radius(X[:10], r, return_distance=False)
 
-            dm = DistanceMetric(metric=metric, **kwargs)
-            D = dm.cdist(Y, X)
+        for i in range(10):
+            ind1 = ind[i]
+            ind2 = np.where(D[i] <= r)[0]
 
-            ind_dm = np.argsort(D, 1)[:, :k]
-            dist_dm = D[np.arange(Y.shape[0])[:, None], ind_dm]
+            ind1.sort()
+            ind2.sort()
 
-            try:
-                assert_array_almost_equal(dist_bt, dist_dm)
-            except:
-                print metric, kwargs
-                assert_array_almost_equal(dist_bt, dist_dm)
+            assert_array_almost_equal(ind1, ind2)
 
+    def test_query_radius_distance(self, n_samples=100, n_features=10):
+        X = 2 * np.random.random(size=(n_samples, n_features)) - 1
+        query_pt = np.zeros(n_features, dtype=float)
 
-def test_ball_tree_p_distance():
-    X = np.random.random(size=(100, 5))
+        eps = 1E-15  # roundoff error can cause test to fail
+        bt = BallTree(X, leaf_size=5)
+        rad = np.sqrt(((X - query_pt) ** 2).sum(1))
 
-    for p in (1, 2, 3, 4, np.inf):
-        bt = BallTree(X, leaf_size=10, metric='minkowski', p=p)
-        kdt = cKDTree(X, leafsize=10)
+        for r in np.linspace(rad[0], rad[-1], 100):
+            ind, dist = bt.query_radius(query_pt, r + eps, return_distance=True)
 
-        dist_bt, ind_bt = bt.query(X, k=5)
-        dist_kd, ind_kd = kdt.query(X, k=5, p=p)
+            ind = ind[0]
+            dist = dist[0]
 
-        assert_array_almost_equal(dist_bt, dist_kd)
+            d = np.sqrt(((query_pt - X[ind]) ** 2).sum(1))
 
+            assert_array_almost_equal(d, dist)
 
-def test_ball_tree_query_radius_count(n_samples=100, n_features=10):
-    X = 2 * np.random.random(size=(n_samples, n_features)) - 1
-
-    dm = DistanceMetric()
-    D = dm.pdist(X, squareform=True)
-
-    r = np.mean(D)
-
-    bt = BallTree(X)
-    count1 = bt.query_radius(X, r, count_only=True)
-
-    count2 = (D <= r).sum(1)
-
-    assert_array_almost_equal(count1, count2)
-
-
-def test_ball_tree_query_radius_indices(n_samples=100, n_features=10):
-    X = 2 * np.random.random(size=(n_samples, n_features)) - 1
-
-    dm = DistanceMetric()
-    D = dm.cdist(X[:10], X)
-
-    r = np.mean(D)
-
-    bt = BallTree(X)
-    ind = bt.query_radius(X[:10], r, return_distance=False)
-
-    for i in range(10):
-        ind1 = ind[i]
-        ind2 = np.where(D[i] <= r)[0]
-
-        ind1.sort()
-        ind2.sort()
-
-        assert_array_almost_equal(ind1, ind2)    
-
-
-def test_ball_tree_query_radius_distance(n_samples=100, n_features=10):
-    X = 2 * np.random.random(size=(n_samples, n_features)) - 1
-    query_pt = np.zeros(n_features, dtype=float)
-
-    eps = 1E-15  # roundoff error can cause test to fail
-    bt = BallTree(X, leaf_size=5)
-    rad = np.sqrt(((X - query_pt) ** 2).sum(1))
-
-    for r in np.linspace(rad[0], rad[-1], 100):
-        ind, dist = bt.query_radius(query_pt, r + eps, return_distance=True)
-
-        ind = ind[0]
-        dist = dist[0]
-
-        d = np.sqrt(((query_pt - X[ind]) ** 2).sum(1))
-
-        assert_array_almost_equal(d, dist)
-
-
-def test_ball_tree_pickle():
-    X = np.random.random(size=(10, 3))
-    bt1 = BallTree(X, leaf_size=1)
-    ind1, dist1 = bt1.query(X)
-    for protocol in (0, 1, 2):
+    def _check_pickle(self, protocol, bt1, ind1, dist1):
         s = cPickle.dumps(bt1, protocol=protocol)
         bt2 = cPickle.loads(s)
-        ind2, dist2 = bt2.query(X)
+        ind2, dist2 = bt2.query(self.X)
         assert np.all(ind1 == ind2)
         assert_array_almost_equal(dist1, dist2)
+        
 
+    def test_pickle(self):
+        bt1 = BallTree(self.X, leaf_size=1)
+        ind1, dist1 = bt1.query(self.X)
+        for protocol in (0, 1, 2):
+            yield (self._check_pickle, protocol, bt1, ind1, dist1)
 
 if __name__ == '__main__':
     import nose
